f5b6b636f8e5d79c4f8f16ca49988aff75c15c2a
Grégoire <72392542+Bassounet@users.noreply.github.com>
2021-11-30 17:46:16 +0100

    ADDCinemachine

---
+++ Assets/Scripts/testCenter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
+
+public class testCenter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+
+    }
+
+    private void OnMouseDown()
+    {
+
+        //.CenterPivot(null);
+        
+    }
+}
