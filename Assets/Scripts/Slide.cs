257682969d143a7158a657b97f7b2f2b0cb2cf5c
VinekNet <72392053+VinekNet@users.noreply.github.com>
2021-12-03 17:03:40 +0100

    LePuzzleMARCHE

--- Assets/Scripts/Slide.cs
+++ Assets/Scripts/Slide.cs
@@ -4,11 +4,98 @@ using UnityEngine;
 
 public class Slide : MonoBehaviour
 {
+    [SerializeField] Material Fiole;
     public string pos;
+    public bool fiole =false;
+    bool Dragging = false;
+
+    float BlockUp = -0.5f;
+    float BlockDown = -4.5f;
+    float BlockLeft = 0.5f;
+    float BlockRight = 4.5f;
+
+    float defUp;
+    float defDown;
+    float defLeft;
+    float defRight;
+
+    public float defX;
+    public float defY;
 
     private Vector3 screenPoint;
     private Vector3 offset;
 
+    private void Awake()
+    {
+        defUp=BlockUp;  
+        defDown=BlockDown;
+        defLeft=BlockLeft;
+        defRight=BlockRight;
+        if (fiole)
+        {
+            //Texture verte
+            gameObject.GetComponent<MeshRenderer>().material= Fiole;
+            //Elle peut déborder en bas
+            BlockDown -= 2;
+            defDown -= 2;
+        }
+        if (pos == "Y")
+        {
+            defX = gameObject.transform.localPosition.x;
+        }
+        if (pos == "X")
+        {
+            defY = gameObject.transform.localPosition.y;
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Block();
+        if (fiole && gameObject.transform.localPosition.y < -5 && !Dragging)
+        {
+            Debug.Log("win!");
+        }
+    }
+    void Block()
+    {
+        //Y
+        if (pos == "Y" && gameObject.transform.localPosition.y > BlockUp)
+        {
+            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, BlockUp, 0);
+        }
+        if (pos == "Y" && gameObject.transform.localPosition.y < BlockDown)
+        {
+            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, BlockDown, 0);
+        }
+        //X
+        if (pos == "X" && gameObject.transform.localPosition.x < BlockLeft)
+        {
+            gameObject.transform.localPosition = new Vector3(BlockLeft, gameObject.transform.localPosition.y, 0);
+        }
+        if (pos == "X" && gameObject.transform.localPosition.x > BlockRight)
+        {
+            gameObject.transform.localPosition = new Vector3(BlockRight, gameObject.transform.localPosition.y, 0);
+        }
+        //FIGER SUR L'AXE
+        if (pos == "Y")
+        {
+            gameObject.transform.localPosition = new Vector3(defX, gameObject.transform.localPosition.y, 0);
+        }
+        if (pos == "X")
+        {
+            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, defY, 0);
+        }
+
+    }
     void OnMouseDown()
     {
         if (pos == "Y")
@@ -27,15 +114,16 @@ public class Slide : MonoBehaviour
 
     void OnMouseDrag()
     {
+        Dragging = true;
         if (pos == "Y")
         {
-            Vector3 curScreenPoint = new Vector3(gameObject.transform.localPosition.x, Input.mousePosition.y, screenPoint.z);
+            Vector3 curScreenPoint = new Vector3(defX, Input.mousePosition.y, screenPoint.z);
             Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
             transform.position = curPosition;
         }
         if (pos == "X")
         {
-            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, gameObject.transform.localPosition.y, screenPoint.z);
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, defY, screenPoint.z);
             Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
             transform.position = curPosition;
         }
@@ -44,47 +132,84 @@ public class Slide : MonoBehaviour
     {
         // If your mouse hovers over the GameObject with the script attached, output this message
         Debug.Log("Drag ended!");
+        Dragging = false;
         if (pos == "Y")
         {
-            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, Mathf.Round(gameObject.transform.localPosition.y - 0.25f) + 0.5f, 0);
+            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, Mathf.Round(gameObject.transform.localPosition.y - 0.5f) + 0.5f, 0);
         }
         if (pos == "X")
         {
-            gameObject.transform.localPosition = new Vector3(Mathf.Round(gameObject.transform.localPosition.x - 0.25f) + 0.5f, gameObject.transform.localPosition.y, 0);
+            gameObject.transform.localPosition = new Vector3(Mathf.Round(gameObject.transform.localPosition.x - 0.5f) + 0.5f, gameObject.transform.localPosition.y, 0);
+        }
+        BlockUp = defUp;
+        BlockDown = defDown;
+        BlockLeft = defLeft;
+        BlockRight = defRight;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (CompareTag("Player") && Dragging)
+        {
+            if (pos == "X")
+            {
+                if (collision.gameObject.GetComponent<Slide>().pos == "Y")
+                {
+                    if (gameObject.transform.localPosition.x < collision.transform.localPosition.x)
+                    {
+                        BlockRight = collision.transform.localPosition.x - 1.5f;
+                        Debug.Log("from gauche");
+                    }
+                    if (gameObject.transform.localPosition.x > collision.transform.localPosition.x)
+                    {
+                        BlockLeft = collision.transform.localPosition.x + 1.5f;
+                        Debug.Log("from droite");
+                    }
+                }
+                if(collision.gameObject.GetComponent<Slide>().pos == "X")
+                {
+                    if (gameObject.transform.localPosition.x < collision.transform.localPosition.x)
+                    {
+                        BlockRight = collision.transform.localPosition.x - 2f;
+                        Debug.Log("from gauche");
+                    }
+                    if (gameObject.transform.localPosition.x > collision.transform.localPosition.x)
+                    {
+                        BlockLeft = collision.transform.localPosition.x + 2f;
+                        Debug.Log("from droite");
+                    }
+                }
+            }
+            if (pos == "Y")
+            {
+                if (collision.gameObject.GetComponent<Slide>().pos == "X")
+                {
+                    if (gameObject.transform.localPosition.y < collision.transform.localPosition.y)
+                    {
+                        BlockUp = collision.transform.localPosition.y - 1.5f;
+                        Debug.Log("from down");
+                    }
+                    if (gameObject.transform.localPosition.y > collision.transform.localPosition.y)
+                    {
+                        BlockDown = collision.transform.localPosition.y + 1.5f;
+                        Debug.Log("from up");
+                    }
+                }
+
+                if (collision.gameObject.GetComponent<Slide>().pos == "Y")
+                {
+                    if (gameObject.transform.localPosition.y < collision.transform.localPosition.y)
+                    {
+                        BlockUp = collision.transform.localPosition.y - 2f;
+                        Debug.Log("from down");
+                    }
+                    if (gameObject.transform.localPosition.y > collision.transform.localPosition.y)
+                    {
+                        BlockDown = collision.transform.localPosition.y + 2f;
+                        Debug.Log("from up");
+                    }
+                }
+            }
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        Block();
-    }
-    void Block()
-    {
-        //Y
-        if (pos == "Y" && gameObject.transform.localPosition.y > -0.5f)
-        {
-            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, -0.5f, 0);
-        }
-        if (pos == "Y" && gameObject.transform.localPosition.y < -4.5f)
-        {
-            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, -4.5f, 0);
-        }
-        //X
-        if (pos == "X" && gameObject.transform.localPosition.x < 0.5f)
-        {
-            gameObject.transform.localPosition = new Vector3(0.5f, gameObject.transform.localPosition.y, 0);
-        }
-        if (pos == "X" && gameObject.transform.localPosition.x > 4.5f)
-        {
-            gameObject.transform.localPosition = new Vector3(4.5f, gameObject.transform.localPosition.y, 0);
-        }
-    }
 }
